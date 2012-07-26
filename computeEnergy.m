5ed29649066bee3e435639148bc36dd6a28bc626
A Humayun <ahumayun@wallis.(none)>
2012-07-26 19:53:27 -0400

    computeEnergy() can now deal with terms whose weight has been set to zero

--- computeEnergy.m
+++ computeEnergy.m
@@ -3,20 +3,32 @@ function [energy, dEnergy] = computeEnergy(r, alpha, alpha_cntr, data, settings)
 full_r = zeros(size(data.mask));
 full_r(data.mask) = r;
 
+energy = 0;
+dEnergy = zeros(size(r));
+
 % compute the shading prior energy
-[Es, dEs] = computeShadingPrior(data.Im, full_r, data.L, data.mask);
+if settings.w_s ~= 0
+    [Es, dEs] = computeShadingPrior(data.Im, full_r, data.L, data.mask);
+
+    energy = energy + settings.w_s*Es;          % add energy as a weighted sum
+    dEnergy = dEnergy + settings.w_s*dEs;       % add energy's partial derivative
+end
 
 % compute gradient consistency
-[Eret, dEret] = computeGradientConsistency(full_r, data.log_gradm_g, data.g2, data.cret_deriv_term, data.L, data.nghb_masks, data.mask);
+if settings.w_r ~= 0
+    [Eret, dEret] = computeGradientConsistency(full_r, data.log_gradm_g, data.g2, data.cret_deriv_term, data.L, data.nghb_masks, data.mask);
+
+    energy = energy + settings.w_r*Eret;        % add energy as a weighted sum
+    dEnergy = dEnergy + settings.w_r*dEret;     % add energy's partial derivative
+end
 
 % compute global sparse reflectance prior
-[Ecl, dEcl] = computeGlobalReflectancePrior(full_r, data.Rd, alpha, alpha_cntr, settings.C, data.mask);
+if settings.w_cl ~= 0
+    [Ecl, dEcl] = computeGlobalReflectancePrior(full_r, data.Rd, alpha, alpha_cntr, settings.C, data.mask);
 
-% complete energy as a weighted sum
-energy = settings.w_s*Es + settings.w_r*Eret + settings.w_cl*Ecl;
-
-% compute partial derivative of energy with respect to elements of r
-dEnergy = settings.w_s*dEs + settings.w_r*dEret + settings.w_cl*dEcl;
+    energy = energy + settings.w_cl*Ecl;        % add energy as a weighted sum
+    dEnergy = dEnergy + settings.w_cl*dEcl;     % add energy's partial derivative
+end
 
 % project the gradient back such that mean(r) does not change
 dEnergy = dEnergy - mean(dEnergy(:));
